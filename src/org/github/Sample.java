bf3107eef03f969856fb992eb02538afe1643de8
meenuanusiya <meenuanusiya1999@gmail.com>
2022-01-31 20:26:52 +0530

    branch3 coding

--- src/org/github/Sample.java
+++ src/org/github/Sample.java
@@ -2,9 +2,11 @@ package org.github;
 
 public class Sample {
 
-	
 	private void f1() {
-System.out.println("f1 is the first line");
+		System.out.println("f1 is the first line");
+	}
+
+	private void branch3() {
+		System.out.println("branch3 coding");
 	}
 }
-
