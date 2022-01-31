ee9b576f1ddcf239c1dbb1baab0a1ef5af4a21e1
meenuanusiya <meenuanusiya1999@gmail.com>
2022-01-31 20:16:30 +0530

    branch2 done coding

--- src/org/github/Sample.java
+++ src/org/github/Sample.java
@@ -2,9 +2,11 @@ package org.github;
 
 public class Sample {
 
-	
 	private void f1() {
-System.out.println("f1 is the first line");
+		System.out.println("f1 is the first line");
+	}
+
+	private void branch2() {
+		System.out.println("Branch2 coding");
 	}
 }
-
